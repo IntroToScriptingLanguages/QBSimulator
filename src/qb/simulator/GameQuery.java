4f009e877f06f66107dbef19aea11f9e4e9505f0
IntroToScriptingLanguages <zahntastic@gmail.com>
2016-04-24 08:37:30 -0500

    Completed GameInterpreter, updated GameQuery and driver

--- src/qb/simulator/GameQuery.java
+++ src/qb/simulator/GameQuery.java
@@ -5,36 +5,78 @@
  */
 package qb.simulator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  *
  * @author Leo
  */
-public class GameQuery {
+public class GameQuery{
   private String query;
-  private String[][] results;
+  private String[][] results; //First element is expected response, rest are messages
   private GameQuery[] next_queries;
+  private boolean CUSTOM_RETRY_BEHAVIOR; //Retries will be handled by the last compartment of results, one will be added to incrementor in that case
   
-  public GameQuery(String q, String[][] r, GameQuery[] n_q)
+  public GameQuery (String q, String[][] r, GameQuery[] n_q, boolean CRB)
   {
     query = q;
     results = r;
     next_queries = n_q;
+    CUSTOM_RETRY_BEHAVIOR = CRB;
   }
   
+  //ReadOnly here
   public String getQuery()
   {
-      return query;
+    return query;
   }
   
   public String[][] getResults()
   {
-      return results;
+		return results;
   }
   
   public GameQuery[] getNextQueries()
-  {
+    {
       return next_queries;
-  }
+    }
+    
+    public boolean customRetryBehavior()
+    {
+      return CUSTOM_RETRY_BEHAVIOR;
+    }
   
-  //GameQuery should probably be read-only, so no setters
+  public static GameQuery parseCSV(BufferedReader read) throws IOException
+    {
+        //Each line in a CSV is a GameQuery
+      	String line = null;
+        
+        while ((line = read.readLine()) != null)
+        {
+          //Index 0: query, Index 1: array of next queries, Index 2: Whether custom response text is enabled, Index 3 to 3 + (number of responses-1): response arrays
+          //If custom response text is enabled, we go up to index 3 + number of responses instead.
+           String[] gameQueryData = line.split(";"); //Change delimiter as needed
+           String query = gameQueryData[0];
+           String[] nextQueries = gameQueryData[1].split("|"); //The number of entires in this should be the number of responses if customText is false, and the number of responses plus 1 if customText is true
+           boolean customText = (gameQueryData[2].equals("T") || gameQueryData[2].equals("True"));
+          
+           String[][] results = new String[nextQueries.length][];
+            
+           //Storing the responses into the result array
+              for (int i=0; i<nextQueries.length - 1; i++)
+              {
+                  results[i] = gameQueryData[3+i].split("|");
+              }
+          
+          //Next game queries:
+          //TODO
+        }
+    }
+  
+  //Recursive helper function the creates all the gameQueries
+  private static GameQuery createGameQuery(String info)
+  {
+    //TODO
+  }
 }
--- src/qb/simulator/QBSimulator.java
+++ src/qb/simulator/QBSimulator.java
@@ -1,21 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//This is a basic engine for a console-based text adventure game with branching choices and multiple endings.
+
+//CSV INSTRUCTIONS:  VERY IMPORTANT:
+
+//The CSV file is divided into lines separated by newline, each line representing a single Query.
+//Two delimiters are used: primary delimiter will be semicolon ";", secondary delimiter will be pipe "|", do not use these characters in your text.
+//Each line is broken up into strings separated by ";".  Strings can be further broken up into substrings separated by "|".  A bunch of substrings with "|" form a single string.
+//The first string in each query is the question being asked.  The second string contains substrings that correspond to the possible next queries.
+//The third string contains a boolean value "T" or "F".  Marking this at T will enable custom behavior if an unrecognized response is inputted (more on that later)
+//The remaining strings contain the responses and the story being expanded after each response. 
+//The first substring in each response string is the expected response, the remaining substrings are the story that would be told after the response is received (line break acts as a newline).
+//After reading through the substrings in the story string, the Query will continue to the line corresponding that response branch.  Exception if one of the branch substrings is marked "ENDING_GOOD", or "ENDING_BAD", which causes the ending to start
+//The line corresponding to the result branch is marked in the second string in the line
+//If the third string is T and custom behavior is enabled, if the user inputted a wrong entry there will be an additional string appended at the end of the line, as well as n additional substring in the second string.  This corresponds to the branch that will happen if input is not recognized
+
+//CSV QUERY LINE EXAMPLE:
+
+//Would you like some cake?;01|02|00;T;Yes|The cake is a lie!|Thank you for visiting Aperture Science;No|OK|Visit us if you still want cake;I did not understand you request|Please write it again
+
+//Here, the user is asked "Would you like some cake?".  If he answers yes, "The cake is a lie!" and "Thank you for visiting Aperture Science" will appear, and the next query will be line 01 (the second line)
+//If he answers no, "OK" and "Visit us if you still want cake" will appear, the next query is line 02.
+//If he answers anything else, "I did not understand you request" and "Please write it again" will appear, the next query is line 00 (so this one will just be repeated in this instance).  In addition, the counter "loop" will be incremented
+
+//ANOTHER QUERY LINE EXAMPLE:
+//Would you like some milk?;00|02|03;F;Yes|You got the milk!|The end!|ENDING_GOOD;No|You did not get the milk!|Keep going!;Maybe|Maybe is a baby|It needs to be coerced until it says yes.
+
+//Here the user is prompted "Would you like some milk?".
+//If he answers yes, "You got the milk!" and "The end!" shows.  Because the next prompt is "ENDING_GOOD", the good ending is immediately displayed and the game ends.
+//If he answers no, "You did not get the milk", and "Keep going" goes, so the story loops to line 02.  If he answers maybe, the messages from the maybe branch is displayed
+
 package qb.simulator;
 
-/**
- *
- * @author Leo
- */
+import java.io.*;
+import java.nio.file.*;
+
 public class QBSimulator {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
+   public static void main(String[] args)
+    {
+      try
+      {
+        //Needed for file IO
+        Path p = Paths.get("./story.csv");
+        InputStream in = Files.newInputStream(p);
+        BufferedReader read = new BufferedReader(new InputStreamReader(in));
+
+        //When the game is running
+        GameQuery start = GameQuery.parseCSV(read);
+        GameInterpreter game = new GameInterpreter(start);
+        boolean replay = false;
+        do
+        {
+              replay = runGame();
+        }
+        while (!replay);
+      }
+      catch (IOException e)
+      {
+          System.out.println("Error: cannot read input file!");
+          e.printStackTrace();
+      }
     }
     
 }
